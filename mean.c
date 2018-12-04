7ff55ae75e8501e0384c622cdc399469c031a381
Anis <anis.peysieux@ens.uvsq.fr>
2018-12-04 14:20:05 +0100

    save works

--- mean.c
+++ mean.c
@@ -3,6 +3,33 @@
 #include <hdf5.h>
 #include <hdf5_hl.h>
 #include <string.h>
+#include <assert.h>
+
+int save_in_hdf5(hid_t file_id, char* filename, char* dataset_name, hsize_t dsize,double* buf)
+{
+  assert(filename != NULL);
+  assert(dataset_name != NULL);
+  assert(buf != NULL);
+  
+  //Create file
+
+  //Create dataspaces
+  hid_t dataspace = H5Screate_simple(1, &dsize, NULL); if(dataspace < 0) { return -2; }
+
+  //Creating datasets
+  hid_t dataset = H5Dcreate(file_id, dataset_name, H5T_NATIVE_DOUBLE, dataspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+  if(dataset < 0) { return -3; }
+
+  //Writing in datasets
+  if(H5Dwrite(dataset, H5T_NATIVE_DOUBLE, dataspace, dataspace, H5P_DEFAULT, buf) < 0) { return -4; }
+
+  //Closing ressources
+  if(H5Sclose(dataspace) < 0) { return -5; }
+  if(H5Dclose(dataset) < 0) { return -6; }
+  //if(H5Fclose(file_id) < 0) { return -1; }
+
+  return 0;
+}
 
 int main()
 {
@@ -75,12 +102,24 @@ int main()
   }
   printf("\n");
   printf("mean: %f\n", last_mean);
+
+
+  hid_t file_id_2 = H5Fcreate("diag.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+  if(file_id < 0) { return -1; }
+  int ret_save_1 = save_in_hdf5 (file_id_2, "diag.h5", "/x_mean", last_dims[0], last_mean_rows);
+  int ret_save_2 = save_in_hdf5 (file_id_2, "diag.h5", "/y_mean", last_dims[1], last_mean_cols);
+  int ret_save_3 = save_in_hdf5 (file_id_2, "diag.h5", "/mean", 1, &last_mean);
+  printf("%d\n", ret_save_1);
+  printf("%d\n", ret_save_2);
+  printf("%d\n", ret_save_3);
+
   
   free(last_mean_rows);
   free(last_mean_cols);
   free(last_buf);
   H5Dclose(last_dset);
   H5Fclose(file_id);
+  H5Fclose(file_id_2);
   return 0;
 //  H5Dread (last, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT,  &readBuf[0]);
 }
